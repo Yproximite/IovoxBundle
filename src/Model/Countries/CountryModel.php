8c99b4447e9156b4df38076ff3bbdfd53c605355
RomulusED69 <romain@yproximite.com>
2021-09-26 09:32:48 +0200

    feat(Countries): add GetCountries

---
+++ src/Model/Countries/CountryModel.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yproximite\IovoxBundle\Model\Countries;
+
+use Yproximite\IovoxBundle\Model\AbstractModel;
+
+class CountryModel extends AbstractModel
+{
+    private function __construct(public int $code, public string $name)
+    {
+    }
+
+    public static function create(array $opts): self
+    {
+        return new self(
+            (int) $opts['code'],
+            $opts['name'],
+        );
+    }
+}
