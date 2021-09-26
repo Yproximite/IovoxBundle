a40e916b336fcd2bbb17f8cb6f9ef9890ddf8340
RomulusED69 <romain@yproximite.com>
2021-09-26 14:10:59 +0200

    fix(Rules): GetBlockedNumbers -> format rules

--- src/Model/Rules/RuleBlockedNumberModel.php
+++ src/Model/Rules/RuleBlockedNumberModel.php
@@ -8,17 +8,14 @@ use Yproximite\IovoxBundle\Model\AbstractModel;
 
 class RuleBlockedNumberModel extends AbstractModel
 {
-    /**
-     * @param array<int, string> $links
-     */
-    private function __construct(public array $links, public ?string $timeTemplate, public ?string $blockingType)
+    private function __construct(public ?string $link, public ?string $timeTemplate, public ?string $blockingType)
     {
     }
 
     public static function create(array $opts): self
     {
         return new self(
-            $opts['links']['link_id'] ?? [],
+            $opts['links']['link_id'] ?? null,
             $opts['time_template'],
             $opts['blocking_type'],
         );
